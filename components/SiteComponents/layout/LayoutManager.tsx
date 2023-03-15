274eea354b2784fa715b153bf04ce22a3a840a29
hadisa <afg.norozi@gmail.com>
2023-03-15 05:10:15 -0700

    -

--- components/SiteComponents/layout/LayoutManager.tsx
+++ components/SiteComponents/layout/LayoutManager.tsx
@@ -1,21 +1,16 @@
 import { useAppStateProvider } from "@appProvider/AppStateProvider";
-import { Skeleton } from "@mui/material";
-import dynamic from "next/dynamic";
 import { GLOBAL_DESCRIPTION, GLOBAL_TITLE, NOTE_GLOBE_SECTION } from "../constant";
 import GlobalEdgeNetwork from "../globe/GlobalEdgeNetwork";
-import GrassIframe from "../grass/GrassIframe";
-import IframeFood from "../iframeFood/IframeFood";
-import IframeLaser from "../iframeLaser/IframeLaser";
 import Layout from "./Layout";
 
 export const LayoutManger = () => {
     const { currentMenu, setCurrentMenu }: any = useAppStateProvider();
     const handleMenu = () => {
         switch (currentMenu) {
-            case "ABOUT US":
-                return <IframeFood />
-            case "SERVICE":
-                return <GrassIframe />
+            // case "ABOUT US":
+            //     return <IframeFood />
+            // case "SERVICE":
+            //     return <GrassIframe />
             // case "CONTACT US":
             //     return <IframeLaser />
             default:
